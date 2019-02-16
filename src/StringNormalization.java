b8baa6dd97088b09f25557643cf2b19f458b44ba
nareshkumar.m <naresh.zeus18@gmail.com>
2019-02-16 00:25:21 -0800

    Java Practice

--- src/StringNormalization.java
+++ src/StringNormalization.java
@@ -6,11 +6,36 @@ public class StringNormalization {
         String s = "My  Name is @$$H0le";
 
 
-
+/*
 
         String ns=s.toLowerCase().replaceAll("[^a-z0-9]", "");
 
-        System.out.println(ns);
+        System.out.println(ns);*/
+
+
+        String ps = s;
+
+        String ns  = "";
+
+        String rns = "";
+
+
+
+        ns   = ps.toLowerCase().replaceAll("[^a-z0-9]","");
+        rns  = new StringBuilder(ns).reverse().toString();
+
+        if (ns.equals(rns)){
+
+            System.out.println(true);
+        }
+
+        else
+            System.out.println(false);
+
+
+
+
+
 
 
     }
