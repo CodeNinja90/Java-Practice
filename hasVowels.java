a7af6c033ef52e2a78b555276ad27aeeae9cd8fa
CodeNinja90 <naresh.jupiter11@gmail.com>
2024-03-01 13:41:49 -0800

    Create hasVowels.java

---
+++ hasVowels.java
@@ -0,0 +1,15 @@
+public static boolean hasVowels(String str) {
+Set<Character> vowels = new HashSet<>();
+vowels.add('a');
+vowels.add('e');
+vowels.add('i');
+vowels.add('o');
+vowels.add('u');
+
+for (char c : str.toLowerCase().toCharArray()) {
+if (vowels.contains(c)) {
+return true;
+			}
+		}
+
+return false;}
